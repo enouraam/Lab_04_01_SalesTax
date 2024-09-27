4bdf80b6f113d1a50c4af3327a5187258526496a
enouraam <enouraam@mail.uc.edu>
2024-09-27 13:40:50 -0400

    init commit/complete

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double salesTax = 0.05;
+        double purchasePrice = 21;
+        double total = salesTax * purchasePrice;
+        System.out.println("The sales tax of your item is "+ total);
+    }
+}
